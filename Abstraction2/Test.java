3641d9cb67c8ccbb2333c7bad92d8db47786b49b
Maruf Ahammed <marufahammed215@gmail.com>
2025-02-21 18:47:31 +0600

    Done

--- Abstraction2/Test.java
+++ Abstraction2/Test.java
@@ -1,19 +1,16 @@
-
-
-
 public class Test {
 
     public static void main(String[] args) {
-        
+
         Shape shape;
 
-        shape = new Rectangle(10,20);
+        shape = new Rectangle(10, 20);
         shape.Area();
-        shape = new Triangle(10,20);
+        shape = new Triangle(10, 20);
         shape.Area();
-        shape = new Circle(10,10);
+        shape = new Circle(10, 10);
         shape.Area();
 
     }
-   
+
 }
